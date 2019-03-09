fccbffc1c7d95881158ff0cdf8a474a5dcb93163
rudolf <rudolf.flodur@gmail.com>
2019-03-09 12:06:14 -0400

    tarea de laboratorio 4 concluido

--- ssiAngular/src/app/app.component.ts
+++ ssiAngular/src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>{{title}}</h1>`,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
--- ssiAngular/src/app/app.module.ts
+++ ssiAngular/src/app/app.module.ts
@@ -2,54 +2,58 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {
   MatAutocompleteModule,
   MatButtonModule,
+  MatButtonToggleModule,
   MatCardModule,
+  MatCheckboxModule,
   MatChipsModule,
+  MatDatepickerModule,
   MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
   MatIconModule,
   MatInputModule,
   MatListModule,
   MatMenuModule,
   MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
   MatRadioModule,
   MatRippleModule,
   MatSelectModule,
   MatSidenavModule,
   MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
   MatSortModule,
   MatTableModule,
   MatTabsModule,
   MatToolbarModule,
   MatTooltipModule
 } from '@angular/material';
-import {MatButtonToggleModule} from '@angular/material/typings/esm5/button-toggle';
-import {MatCheckboxModule} from '@angular/material/typings/esm5/checkbox';
-import {MatDatepickerModule} from '@angular/material/typings/esm5/datepicker';
-import {MatExpansionModule} from '@angular/material/typings/esm5/expansion';
-import {MatGridListModule} from '@angular/material/typings/esm5/grid-list';
 import {FlexLayoutModule} from '@angular/flex-layout';
-import {MatSnackBarModule} from '@angular/material/typings/esm5/snack-bar';
-import {MatSlideToggleModule} from '@angular/material/typings/esm5/slide-toggle';
-import {MatProgressSpinnerModule} from '@angular/material/typings/esm5/progress-spinner';
-import {MatProgressBarModule} from '@angular/material/typings/esm5/progress-bar';
-import {MatPaginatorModule} from '@angular/material/typings/esm5/paginator';
 import {CatalogComponent} from './catalog/catalog.component';
+import {ItemDetailComponent} from './item-detail/item-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CatalogComponent
+    CatalogComponent,
+    ItemDetailComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, BrowserAnimationsModule,
     MatAutocompleteModule, MatButtonModule, MatButtonToggleModule, MatCardModule, MatCheckboxModule, MatChipsModule, MatDatepickerModule,
     MatDialogModule, MatExpansionModule, MatGridListModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule,
     MatNativeDateModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule, MatRadioModule, MatRippleModule,
     MatSelectModule, MatSidenavModule, MatSliderModule, MatSlideToggleModule, MatSnackBarModule, MatSortModule, MatTableModule,
     MatTabsModule, MatToolbarModule, MatTooltipModule,
     FlexLayoutModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
